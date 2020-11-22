ec9da27f0af02bd204b9647dbc1244ed86923a99
andre <andrebertonhadev@gmail.com>
2020-11-22 11:04:04 -0800

    Services e SOLID

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,49 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/**
+ [x] recebimento de informacoes
+ [/] tratativa de erros e excessoes
+ [x] acesso ao repositorio
+ */
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+/**
+dependency inversion (SOLID)
+ */
+
+class CreateAppointmentService {
+
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate
+    );
+
+    if(findAppointmentInSameDate) {
+      throw Error("This appointment is already booked");
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
