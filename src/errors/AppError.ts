ab4957852a2a99314c960e332c1b41842f91741a
andre <andrebertonhadev@gmail.com>
2020-12-22 12:15:39 -0800

    Error class added

---
+++ src/errors/AppError.ts
@@ -0,0 +1,11 @@
+class AppError {
+  public readOnly message: string;
+  public readOnly statusCode: number;
+
+  constructor(message: string, statusCode: 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default AppError;
