f884e9037d428b650887ea2037d833ba93e2cc96
andre <andrebertonhadev@gmail.com>
2020-12-22 13:33:56 -0800

    code review remove try catch from routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -11,25 +11,18 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
+  const { name, email, password } = request.body;
+  const createUser = new CreateUserService();
 
-  try {
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    const { name, email, password } = request.body;
+  delete user.password;
+  return response.json(user);
 
-    const createUser = new CreateUserService();
-
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
 });
 
 usersRouter.patch(
@@ -37,20 +30,15 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      delete user.password;
-
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    delete user.password;
+    return response.json(user);
   },
 );
 
