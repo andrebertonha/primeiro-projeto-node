2cee5954d59eb36fe08b27da19bc5d447b9ca561
andre <andrebertonhadev@gmail.com>
2020-12-22 13:12:01 -0800

    readonly estava readOnly

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -1,6 +1,6 @@
 class AppError {
-  public readOnly message: string;
-  public readOnly statusCode: number;
+  public readonly message: string;
+  public readonly statusCode: number;
 
   constructor(message: string, statusCode=400) {
     this.message = message;
